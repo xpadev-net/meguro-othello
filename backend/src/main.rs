a4da6f43f71bb059ad50d408ab45b55979601715
xpadev <xpadev@gmail.com>
2024-01-17 09:49:04 +0900

    feat: backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,28 @@
 use moon::*;
+use shared::{DownMsg, UpMsg};
 
 async fn frontend() -> Frontend {
     Frontend::new()
         .title("New Project")
         .append_to_head(include_str!("../favicon.html")) // realfavicongenerator.net
         .append_to_head(
-            "
+        "
         <style>
             html {
                 background-color: black;
             }
         </style>",
-        )
+    )
 }
 
-async fn up_msg_handler(_: UpMsgRequest<()>) {}
+async fn up_msg_handler(req: UpMsgRequest<UpMsg>) {
+    println!("{:?}", req);
+
+    let UpMsgRequest { up_msg, cor_id, .. } = req;
+    let UpMsg::SendMessage(message) = up_msg;
+
+    sessions::broadcast_down_msg(&DownMsg::MessageReceived(message), cor_id).await;
+}
 
 #[moon::main]
 async fn main() -> std::io::Result<()> {
